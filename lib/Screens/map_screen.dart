871c0ee58400586bd00b712365544badf3490672
krunal <krunalmiracle@gmail.com>
2021-05-10 04:14:40 +0200

    Planes on the map, have them.
    Rough built

--- lib/Screens/map_screen.dart
+++ lib/Screens/map_screen.dart
@@ -1,6 +1,8 @@
+import 'package:decereix/Provider/cat_provider.dart';
 import 'package:decereix/Screens/show_map_leaflet.dart';
 import 'package:flutter/material.dart';
 import 'package:latlong/latlong.dart';
+import 'package:provider/provider.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key key}) : super(key: key);
@@ -13,9 +15,47 @@ class _MapScreenState extends State<MapScreen> {
   LatLng locationBCN = new LatLng(41.29561833, 2.095114167);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ShowMapLeaflet(location: this.locationBCN,
-      ),
+    CatProvider _catProvider = Provider.of<CatProvider>(context, listen: true);
+    /// [_fetchPreferences] We fetch the preference from the storage and notify in future
+    Future<List<LatLng>> _fetchCATALL() async {
+      List<LatLng> points = [];
+      _catProvider.mlatTrajectories.forEach((trayectory) {
+        points.add(trayectory.ListPoints[0]);
+      });
+      debugPrint("Fetch Cat All");
+      return points;
+    }
+
+    //------------------------------------------------------------//
+    final getFutureBuildWidget = FutureBuilder<List<LatLng>>(
+      future: _fetchCATALL(),
+      builder: (context, snapshot1) {
+        switch (snapshot1.connectionState) {
+          case ConnectionState.none:
+
+          /// Show [ErrorScreen], as we are unable to get the response...
+            return Text("Cannot Connect to Server...");
+          case ConnectionState.waiting:
+
+          /// Show [LoadingScreen], as we are waiting for the response...
+            return Text("Loading...");
+          default:
+            if (snapshot1.hasError) {
+              /// Show [ErrorScreen], as we got a error
+              return Text(
+                snapshot1.error.toString(),
+              );
+            } else {
+              //Error in Autologgin --> Login probably -1
+              /// Redirect to [Login]
+              return ShowMapLeaflet(location: this.locationBCN, pois: snapshot1.data,
+
+              );
+            }
+        }
+      },
     );
+
+    return getFutureBuildWidget;
   }
 }
--- lib/Screens/show_map_leaflet.dart
+++ lib/Screens/show_map_leaflet.dart
@@ -5,7 +5,8 @@ import 'package:latlong/latlong.dart';
 const double pi = 3.1415;
 class ShowMapLeaflet extends StatefulWidget {
   final LatLng location;
-  const ShowMapLeaflet({Key key, @required this.location}) : super(key: key);
+  final List<LatLng> pois;
+  const ShowMapLeaflet({Key key, @required this.location, @required this.pois}) : super(key: key);
 
   @override
   _ShowMapLeafletState createState() => _ShowMapLeafletState();
@@ -13,8 +14,6 @@ class ShowMapLeaflet extends StatefulWidget {
 
 class _ShowMapLeafletState extends State<ShowMapLeaflet> {
 
-  /// [logger] to logg all of the logs in console prettily!
-  List<LatLng> tappedPoints = [];
   @override
   void initState() {
     super.initState();
@@ -22,7 +21,7 @@ class _ShowMapLeafletState extends State<ShowMapLeaflet> {
 
   @override
   Widget build(BuildContext context) {
-    var markers = [
+    /*var markers = [
       new Marker(
         width: 60.0,
         height: 60.0,
@@ -37,7 +36,45 @@ class _ShowMapLeafletState extends State<ShowMapLeaflet> {
           ),
         ),
       )
-    ];
+    ];*/
+    List<Marker> markers() {
+      //print(widget.elements);
+      List<Marker> returnMarkers = [];
+      if (widget.pois.isNotEmpty) {
+        widget.pois.forEach((val) => returnMarkers.add(
+            new Marker(
+              width: 60.0,
+              height: 60.0,
+              point: val,
+              builder: (ctx) => Transform.rotate(
+                angle: 180 * pi / 180,
+                child: Icon(
+                  Icons.airplanemode_active,
+                  color: Colors.red,
+                  size: 42.0,
+                  semanticLabel: 'Marker Placed on Map', //For Accessibility
+                ),
+              ),
+            )
+        ));
+      } else {
+        returnMarkers.add( new Marker(
+          width: 60.0,
+          height: 60.0,
+          point: widget.location,
+          builder: (ctx) => Transform.rotate(
+            angle: 180 * pi / 180,
+            child: Icon(
+              Icons.airplanemode_active,
+              color: Colors.red,
+              size: 42.0,
+              semanticLabel: 'Marker Placed on Map', //For Accessibility
+            ),
+          ),
+        ));
+      }
+      return returnMarkers;
+    }
     return SafeArea(
       child: Scaffold(
         body: FlutterMap(
@@ -52,7 +89,7 @@ class _ShowMapLeafletState extends State<ShowMapLeaflet> {
                 keepBuffer: 150,
                 urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", subdomains: ['a', 'b', 'c']),
             new MarkerLayerOptions(
-              markers: markers,
+              markers: markers(),
             ),
           ],
         ),
