8742da371e5be3b026760ad690f950e9739fb6e3
krunal <krunalmiracle@gmail.com>
2021-05-12 00:48:53 +0200

    Speed Forward, Backward with Jump

--- lib/Screens/show_map_leaflet.dart
+++ lib/Screens/show_map_leaflet.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:floating_action_row/floating_action_row.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
@@ -5,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong/latlong.dart';
+import 'package:weight_slider/weight_slider.dart';
 
 import 'error_show.dart';
 
@@ -13,8 +16,8 @@ const double pi = 3.1415;
 class ShowMapLeaflet extends StatefulWidget {
   final List<List<Marker>> markerStack;
   final int lengthMarkerStack;
-  const ShowMapLeaflet(
-      {Key key, @required this.markerStack, @required this.lengthMarkerStack})
+
+  const ShowMapLeaflet({Key key, @required this.markerStack, @required this.lengthMarkerStack})
       : super(key: key);
 
   @override
@@ -23,20 +26,45 @@ class ShowMapLeaflet extends StatefulWidget {
 
 class _ShowMapLeafletState extends State<ShowMapLeaflet> {
   static LatLng location = new LatLng(41.29561833, 2.095114167);
-  /// A [MapController], used to control the map.
-  MapController _mapController = new MapController(
 
-  );
-  int index = 0;
+  /// A [MapController], used to control the map.
+  MapController _mapController = new MapController();
+  Timer timer;
   bool isPlaying = false;
   int initialTime = 86400;
+  static int repetitionDelay = 1000; // milliseconds [ms]
+  int currentSpeed = 1;
   static double minZoom = 6.0;
   static double maxZoom = 18.0;
   static double zoomFactor = 0.5;
+
   /// [ValueNotifier] Value Notifier with Initial Values which only rebuilds the
   /// widgets it is used
   //ValueNotifier<List<String>> universityNames = ValueNotifier(["Select your University:"]);
   ValueNotifier<int> currIndex = ValueNotifier(0);
+
+  void autoPlay() {
+    if (currIndex.value < widget.lengthMarkerStack) {
+      if (widget.lengthMarkerStack == 1) {
+        if (widget.markerStack[0].length != 0) {
+          setState(() {
+            currIndex.value = currIndex.value + 1;
+          });
+        }
+      } else {
+        setState(() {
+          currIndex.value = currIndex.value + 1;
+        });
+      }
+    } else {
+      if (timer != null) {
+        if (timer.isActive) {
+          timer.cancel();
+        }
+      }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -45,51 +73,113 @@ class _ShowMapLeafletState extends State<ShowMapLeaflet> {
   @override
   Widget build(BuildContext context) {
     // Floating Action Panel
-    List<Widget> children = [];
-    children.add(
+    List<Widget> mapControls = [];
+
+    /// To Go 1 second backward in time
+    mapControls.add(
       FloatingActionRowButton(
-        icon: Icon(Icons.arrow_back_rounded),
+        icon: Icon(Icons.fast_rewind),
         onTap: () {
           // All the trajectories must be reloaded for all of the plane for current Time
-          if(currIndex.value>0){
+          if (currIndex.value > 0 &&
+              currIndex.value < widget.lengthMarkerStack) {
             setState(() {
-              currIndex.value = currIndex.value-1;
+              currIndex.value = currIndex.value - 1;
             });
           }
         },
       ),
     );
-    children.add(
+    mapControls.add(
       FloatingActionRowDivider(),
     );
-    children.add(
+
+    /// [Play/Pause] Button for Autoplaying the plane movements
+    mapControls.add(
       FloatingActionRowButton(
         icon: this.isPlaying
-            ? Icon(Icons.play_arrow_rounded)
-            : Icon(Icons.pause_rounded),
+            ? Icon(Icons.pause_rounded)
+            : Icon(Icons.play_arrow_rounded),
         onTap: () {
-          setState(() => this.isPlaying = !this.isPlaying);
+
+          setState(() {
+            this.isPlaying = !this.isPlaying;
+          });
+          if (this.isPlaying) {
+            if (timer != null) {
+              if (timer.isActive) {
+                timer.cancel();
+              }
+            }
+            timer = Timer.periodic(Duration(milliseconds: repetitionDelay),
+                (Timer t) => autoPlay());
+          } else {
+            if (timer != null) {
+              if (timer.isActive) {
+                timer.cancel();
+              }
+            }
+          }
         },
       ),
     );
-    children.add(
+    mapControls.add(
       FloatingActionRowDivider(),
     );
-    children.add(
+
+    /// To Go 1 second forward in Time
+    mapControls.add(
       FloatingActionRowButton(
-        icon: Icon(Icons.arrow_forward),
+        icon: Icon(Icons.fast_forward),
         onTap: () {
-          setState(() {
-            this.index = (this.index) <= widget.lengthMarkerStack
-                ? this.index++
-                : this.index;
-          });
-          if(currIndex.value<widget.lengthMarkerStack){
-            setState(() {
-              currIndex.value = currIndex.value+1;
-            });
+          if (currIndex.value < widget.lengthMarkerStack) {
+            if (widget.lengthMarkerStack == 1) {
+              if (widget.markerStack[0].length != 0) {
+                setState(() {
+                  currIndex.value = currIndex.value + 1;
+                });
+              }
+            } else {
+              setState(() {
+                currIndex.value = currIndex.value + 1;
+              });
+            }
           }
         },
+      ),
+    );
+
+    /// Top Center Floating Panel
+    List<Widget> floatingPanelSpeed = [];
+    floatingPanelSpeed.add(
+      SizedBox(
+        width: 96,
+        height: 96,
+        child: WeightSlider(
+          weight: this.currentSpeed,
+          minWeight: 1,
+          maxWeight: 10,
+          onChange: (val) => setState(() {
+            int timerVal = repetitionDelay ~/ val;
+            if (timer != null) {
+              if (timer.isActive) {
+                timer.cancel();
+                timer = Timer.periodic(
+                    Duration(milliseconds: timerVal), (Timer t) => autoPlay());
+                setState(() {
+                  this.currentSpeed = val;
+                });
+              } else {
+                timer = Timer.periodic(
+                    Duration(milliseconds: timerVal), (Timer t) => autoPlay());
+                setState(() {
+                  this.currentSpeed = val;
+                });
+              }
+            }
+          }),
+          unit: 'x', // optional
+        ),
       ),
     );
     // Flutter Map -- Widget
@@ -121,27 +211,37 @@ class _ShowMapLeafletState extends State<ShowMapLeaflet> {
       child: ErrorShow(errorText: "Unexpected error occured, Please Restart the App"),
     );
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
       floatingActionButton: FloatingActionRow(
-        children: children,
+        children: floatingPanelSpeed,
         color: Colors.blueAccent,
         elevation: 4,
       ),
-      body: SafeArea(
-        child: SizedBox.expand(
-          child: Listener(
-            onPointerSignal: (pointerSignal) {
-              if (pointerSignal is PointerScrollEvent) {
-                LatLng latlng = _mapController.center ?? new LatLng(0,0);
-                /// Scroll Direction Normalized * [Zoom factor] :: [Windows Zoom] precision factor
-                double deltaZoom = (pointerSignal.scrollDelta.direction/(pointerSignal.scrollDelta.direction).abs())*zoomFactor;
-                double zoomNew = _mapController.zoom - deltaZoom;
-                zoomNew = zoomNew<minZoom ? minZoom : zoomNew;
-                zoomNew = zoomNew>maxZoom ? maxZoom : zoomNew;
-                _mapController.move(latlng,zoomNew );
-              }
-            },
-            child: flutterMapWidget
+      body: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionRow(
+          children: mapControls,
+          color: Colors.blueAccent,
+          elevation: 4,
+        ),
+        body: SafeArea(
+          child: SizedBox.expand(
+            child: Listener(
+                onPointerSignal: (pointerSignal) {
+                  if (pointerSignal is PointerScrollEvent) {
+                    LatLng latlng = _mapController.center ?? new LatLng(0, 0);
+
+                    /// Scroll Direction Normalized * [Zoom factor] :: [Windows Zoom] precision factor
+                    double deltaZoom = (pointerSignal.scrollDelta.direction /
+                            (pointerSignal.scrollDelta.direction).abs()) *
+                        zoomFactor;
+                    double zoomNew = _mapController.zoom - deltaZoom;
+                    zoomNew = zoomNew < minZoom ? minZoom : zoomNew;
+                    zoomNew = zoomNew > maxZoom ? maxZoom : zoomNew;
+                    _mapController.move(latlng, zoomNew);
+                  }
+                },
+                child: flutterMapWidget),
           ),
         ),
       ),
